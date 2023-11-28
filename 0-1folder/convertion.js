587dec17fd3d24c88b4ddbd964c1337580896951
MightyRav3n <152253911+MightyRav3n@users.noreply.github.com>
2023-11-28 07:21:41 +0000

    Conversion

---
+++ 0-1folder/convertion.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+let s = 0
+
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+console.log(typeof s)
+let valueIns = Number(s)
+console.log(typeof valueIns)
+console.log(valueIns)
+
+
+//"33"=> 33
+//"33abc"=>NaN
+//true= > 1 
+
+// for typecasting to boolean  1 for true o for false 
+
+x
